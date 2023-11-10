454b93b225d18cd4504a1aa2f8352e4900f67420
suprastinka <zelinska.12221204@stud.op.edu.ua>
2023-11-10 12:41:15 +0200

    Initial commit

---
+++ client/src/components/NavBar.js
@@ -0,0 +1,51 @@
+import React, {useContext} from 'react';
+import {Context} from "../index";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import {NavLink} from "react-router-dom";
+import {ADMIN_ROUTE, LOGIN_ROUTE, SHOP_ROUTE} from "../utils/consts";
+import {Button} from "react-bootstrap";
+import {observer} from "mobx-react-lite";
+import Container from "react-bootstrap/Container";
+import {useHistory} from 'react-router-dom'
+const NavBar = observer(() => {
+    const {user} = useContext(Context)
+    const history = useHistory()
+
+    const logOut = () => {
+        user.setUser({})
+        user.setIsAuth(false)
+    }
+
+    return (
+        <Navbar bg="dark" variant="dark">
+            <Container>
+                <NavLink style={{color:'gold'}} to={SHOP_ROUTE}>EcoGizmo</NavLink>
+                {user.isAuth ?
+                    <Nav className="ml-auto" style={{color: 'aqua'}}>
+                        <Button
+                            variant={"outline-light"}
+                            onClick={() => history.push(ADMIN_ROUTE)}
+                        >
+                            Адмін панель
+                        </Button>
+                        <Button
+                            variant={"outline-light"}
+                            onClick={() => logOut()}
+                            className="ml-2"
+                        >
+                            Вийти
+                        </Button>
+                    </Nav>
+                    :
+                    <Nav className="ml-auto" style={{color: 'white'}}>
+                        <Button variant={"outline-light"} onClick={() => history.push(LOGIN_ROUTE)}>Авторизація</Button>
+                    </Nav>
+                }
+            </Container>
+        </Navbar>
+
+    );
+});
+
+export default NavBar;
